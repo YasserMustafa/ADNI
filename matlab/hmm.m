3d4f74b372278b4be8d54aba148866dde54ac493
Dev <devendragoyal@gmail.com>
2014-12-26 17:27:09 +0530

    Added archived code file

--- matlab/hmm.m
+++ matlab/hmm.m
@@ -1,4 +1,4 @@
-function [model, ll] = pet_hmm()
+function [model, ll] = hmm()
 %% Train a HMM model on the PET data, and visualize the results
 
 %% Read and clean data
@@ -149,26 +149,6 @@ close all;
 
 end
 
-function typeset_trans(A)
-%% Print a LaTeX version of a transition matrix learned by the HMM
-
-%%
-A = cat(3, A{:});
-val = mean(A, 3);
-stdev = std(A, 0, 3);
-fprintf('Transition matrix\n')
-
-for row = 1:size(val, 1)
-    text = '';
-    for col = 1:size(val, 2)
-        text = horzcat([text, ' ', num2str(val(row, col), '%.3f'), ...
-            ' \pm ', num2str(stdev(row, col), '%.3f'), ' &']);
-    end
-    text = horzcat([text(1:end-1), '\\']);
-    disp(text);
-end
-
-end
 
 function [model, path, prob, dist] = getReordered(order, model, path,...
     prob, dist)
@@ -234,54 +214,6 @@ end
         end
         dist = newDist;
     end
-
-end
-
-function trellis = getTrellis(path, K, t_max)
-%% Visualize the Viterbi Trellis
-
-% store the number of transitions from every possible source to every
-% possible destination. The row represents the source, and the column the 
-% destination state. The third dimension 't' represents the transitions seen
-% at time = t.
-trellis = zeros(K, K, t_max-1);
-
-for i=1:numel(path)
-    for t=1:numel(path{i})-1
-        r = path{i}(t);
-        c = path{i}(t+1);
-        trellis(r, c, t) = trellis(r, c, t) + 1;
-    end
-end
-
-end
-
-function plotTrellis(trellis, name)
-
-t_max = size(trellis, 3);
-K = size(trellis, 1);
-
-figure;
-hold on;
-xlim([0, t_max+1])
-ylim([0, K+1])
-xlabel('Time')
-ylabel('HMM State')
-
-for t=1:size(trellis, 3)
-    [r, c] = find(trellis(:, :, t));
-    for i=1:numel(r)
-        trans = trellis(:, :, t);
-        normalizer = sum(trans(:));
-        plot([t, t+1], [r(i), c(i)], ...
-            'Marker', '.', 'MarkerSize', 10, ...
-            'LineStyle', '-', 'LineWidth', 30*trans(r(i), c(i))/normalizer);
-        alpha(0.2);
-    end
-end
-hold off;
-
-title(name);
 
 end
 
